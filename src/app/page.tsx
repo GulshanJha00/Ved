1b955715e6140b97da7c0de1a2455da3f6d6973d
GulshanJha00 <gk4763549@gmail.com>
2024-11-12 11:30:30 +0530

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-screen w-full flex justify-center items-center bg-red-500'>
+      <h1 className='text-8xl'>Hello</h1>
+    </div>
+  )
+}
+
+export default page
