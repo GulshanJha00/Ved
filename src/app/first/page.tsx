f04fd8b9f462413c8a97075a3df8433ce5bd4675
GulshanJha00 <gk4763549@gmail.com>
2024-11-12 23:49:49 +0530

    all year odd even sem added

---
+++ src/app/first/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from '@/components/Button'
+import Navbar from '@/components/Navbar/Navbar'
+
+const page = () => {
+  return (
+    <div>
+      <div>
+      <Navbar />
+      <div className="min-h-screen bg-gradient-to-r from-[#f4f1e1] via-[#d8c9a3] to-[#bfa77e] animate-gradient flex justify-center items-center flex-wrap gap-6 py-8 ">
+        <Button text="Odd Sem" href="/firstodd" />
+        <Button text="Even Sem" href="/firsteven" />
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default page
