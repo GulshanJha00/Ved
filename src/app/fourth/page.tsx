b9ce9c307302f03bee69687dfdebccd07e541a7c
GulshanJha00 <gk4763549@gmail.com>
2024-11-14 00:52:43 +0530

    3rd and 4th sem added

--- src/app/fourth/page.tsx
+++ src/app/fourth/page.tsx
@@ -8,11 +8,16 @@ const page = () => {
       <div>
       <Navbar />
       <div className="min-h-screen bg-gradient-to-r from-[#f4f1e1] via-[#d8c9a3] to-[#bfa77e] animate-gradient flex justify-center items-center flex-wrap gap-6 py-8 ">
-        <Button text="ADA" href="/" />
-        <Button text="Advanced Java" href="/" />
-        <Button text="Graph Theory" href="/" />
-        <Button text="DBMS" href="/" />
-        <Button text="Biology" href="/" />
+        <Button text="ADA" href="https://drive.google.com/drive/folders/1OiUocPtnIExDd8h0cQ19gABdWlEO9R1e?usp=sharing" />
+
+        <Button text="Advanced Java" href="https://drive.google.com/drive/folders/1IjkvmIyfzHYwyNW2kZs-mjWLjyMvk_VR?usp=sharing" />
+        
+        <Button text="Biology" href="https://drive.google.com/drive/folders/1X_uqwc_SOG_I3Q1G_qbrjMLJ-Ov5cOW-?usp=sharing" />
+
+        <Button text="Graph Theory" href="https://drive.google.com/drive/folders/1dzHn2vK4JezUMWTRRwtRwKSSoQqXaP7I?usp=sharing" />
+
+        <Button text="DBMS" href="https://drive.google.com/drive/folders/1UwIEXIEvofZVAmnMqpy2t9G5ATI6_kGs?usp=sharing" />
+
       </div>
     </div>
     </div>
--- src/app/third/page.tsx
+++ src/app/third/page.tsx
@@ -8,11 +8,15 @@ const page = () => {
       <div>
       <Navbar />
       <div className="min-h-screen bg-gradient-to-r from-[#f4f1e1] via-[#d8c9a3] to-[#bfa77e] animate-gradient flex justify-center items-center flex-wrap gap-6 py-8 ">
-        <Button text="DDCO" href="https://drive.google.com/drive/folders/1VZGJ4KgmMSiaQcsR4cwkz_WZ3VUjakRN?usp=sharing" />
-        <Button text="DSA" href="/" />
-        <Button text="Mathematics-III" href="/" />
-        <Button text="Operating System" href="/" />
-        <Button text="OOP java" href="/" />
+        <Button text="DDCO" href="https://drive.google.com/drive/folders/1-ZnDLbbIIpkvtjYU6cpdMyriqjkGaqt6?usp=sharing" />
+
+        <Button text="DSA" href="https://drive.google.com/drive/folders/1EdtJiy0SjhLjAPj_5uOcd1pG2dKXEBzZ?usp=sharing" />
+
+        <Button text="Mathematics-III" href="https://drive.google.com/drive/folders/1toWgiyLJy0GrRODtVYsAEviHuaPEkiKq?usp=sharing" />
+
+        <Button text="Operating System" href="https://drive.google.com/drive/folders/1oTvuQ7_BrFdx8-LTT5balNA2RV_5J9W9?usp=sharing" />
+
+        <Button text="OOP java" href="https://drive.google.com/drive/folders/1JWo6qJmrYavMpYpVzYkuhhmY2MwMEOPt?usp=sharing" />
       </div>
     </div>
     </div>
