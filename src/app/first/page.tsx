21021605acc7e5a71b619c6a451671cf8e79b0a3
GulshanJha00 <gk4763549@gmail.com>
2024-11-14 01:07:06 +0530

    1st and 2nd updated

--- src/app/first/page.tsx
+++ src/app/first/page.tsx
@@ -8,10 +8,12 @@ const page = () => {
       <div>
       <Navbar />
       <div className="min-h-screen bg-gradient-to-r from-[#f4f1e1] via-[#d8c9a3] to-[#bfa77e] animate-gradient flex justify-center items-center flex-wrap gap-6 py-8 ">
-        <Button text="CAED" href="/" />
-        <Button text="Chemistry" href="/" />
+        <Button text="CAED" href="https://drive.google.com/drive/folders/1w-x2dPdnbZDIJxmU3wtaZAL8zb3H1utq?usp=sharing" />
+
+        <Button text="Chemistry" href="https://drive.google.com/drive/folders/12ryTRVMEkI1xkwh5CMsKkI4JI_4hI6r7?usp=sharing" />
         <Button text="Electrical" href="/" />
-        <Button text="Mathematics" href="/" />
+
+        <Button text="Mathematics" href="https://drive.google.com/drive/folders/1hOSKP7WVqGwnsk4yARCBmt81WS2NnjMK?usp=sharing" />
         <Button text="Mechanical" href="https://drive.google.com/drive/folders/1QgC1PZcyLGhjGK8fOByv2twD0r9CmTds?usp=sharing" />
         <Button text="Python" href="https://drive.google.com/drive/folders/1rwu3e3GeC67XUWpyTbUUzR1DEGeD-SnG?usp=sharing" />
         
--- src/app/second/page.tsx
+++ src/app/second/page.tsx
@@ -9,14 +9,16 @@ const page = () => {
       <Navbar />
       <div className="min-h-screen bg-gradient-to-r from-[#f4f1e1] via-[#d8c9a3] to-[#bfa77e] animate-gradient flex justify-center items-center flex-wrap gap-6 py-8 ">
         
+        <Button text="C-programming" href="https://drive.google.com/drive/folders/1He2zY8SmkSOtzGrQ_Wm5ZDxMs7MygS5p?usp=sharing" />
+
+        <Button text="Cyber Security" href="https://drive.google.com/drive/folders/1YsPpHNZboZAPdXyJDJbu4P9VAJFE3hJP?usp=sharing" />
+
         <Button text="Electronics" href="https://drive.google.com/drive/folders/1n1yaZMp784tCQtXv0bIJ0tHVzuaq7pKb?usp=sharing" />
 
         <Button text="Physics" href="https://drive.google.com/drive/folders/1TWDTNyOG3ydA2cryAkZVDNmlgnvxua4F?usp=sharing" />
 
-        <Button text="Mathematics-II" href="/" />
+        <Button text="Mathematics-II" href="https://drive.google.com/drive/folders/1QmE0GCmXchMP4cNcC3P5jOE5jpVN1jJM?usp=sharing" />
 
-        <Button text="C-programming" href="https://drive.google.com/drive/folders/1He2zY8SmkSOtzGrQ_Wm5ZDxMs7MygS5p?usp=sharing" />
-        <Button text="Cyber Security" href="https://drive.google.com/drive/folders/1YsPpHNZboZAPdXyJDJbu4P9VAJFE3hJP?usp=sharing" />
       </div>
     </div>
     </div>
