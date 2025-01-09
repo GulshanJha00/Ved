e0fcb7e7b45fa5d85520a0602e077543a6b1315d
DipanshuShaw <dip100shaw@gmail.com>
2025-01-09 23:11:09 +0530

    roadmap test

---
+++ src/app/roadmap/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <iframe className=' w-full h-screen border-0' src="https://whimsical.com/embed/L1xeQvc7avPuZX5jbrR5er"/>
+    </div>
+  )
+}
+
+export default page
