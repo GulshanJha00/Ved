4297e3b5097fe9e108812641d740d52d1b5930e8
GulshanJha00 <gk4763549@gmail.com>
2025-01-14 00:53:25 +0530

    changes applied

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics */}
+        
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5826477007107638"
      crossOrigin="anonymous"></script>
+
 
         <meta
           name="google-adsense-account"
