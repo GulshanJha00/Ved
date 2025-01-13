2710d0e528b5f4489b06dab73505ebdd88341371
GulshanJha00 <gk4763549@gmail.com>
2025-01-14 01:01:08 +0530

    changes 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5826477007107638"
+     crossOrigin="anonymous"></script>
+
+<ins className="adsbygoogle"
+     style={{display:"block"}}
+     data-ad-client="ca-pub-5826477007107638"
+     data-ad-slot="5156871218"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5826477007107638"
      crossOrigin="anonymous"></script>
         <meta
