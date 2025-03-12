ead652d9a9fa26b3e24f6f1f136f565f5d368299
DipanshuShaw <dip100shaw@gmail.com>
2025-03-12 11:30:31 +0530

    minor edits

--- src/app/flexbox-game/page.tsx
+++ src/app/flexbox-game/page.tsx
@@ -9,12 +9,12 @@ const Page = () => {
   const myText = "Explore";
 
   return (
-    <div className="flex h-[88.4vh] justify-center items-center testClass text-white ">
+    <div className="flex h-full sm:h-[calc(100vh-5rem)] justify-center items-center testClass text-white ">
         <div className='text-center'>
-          <h1 className='text-5xl rowdies-bold my-4'>Welcome to <span className='head-animated-flexbox bg-clip-text text-transparent border-b-4 rounded-sm'>Ved Flex!</span></h1>
-          <div className='rowdies-regular w-2/3 mx-auto leading-8'>Ved Flex transforms learning Flexbox into an engaging adventure where you guide a character to its destination by writing the correct CSS properties. Each level introduces new Flexbox concepts, helping you understand layout alignment through real-time animations.</div>
+          <h1 className='md:text-5xl  text-3xl rowdies-bold my-4'>Welcome to <span className='head-animated-flexbox bg-clip-text text-transparent border-b-4 rounded-sm'>Ved Flex!</span></h1>
+          <div className='rowdies-regular md:w-2/3 mx-auto leading-8 max-md:text-sm'>Ved Flex transforms learning Flexbox into an engaging adventure where you guide a character to its destination by writing the correct CSS properties. Each level introduces new Flexbox concepts, helping you understand layout alignment through real-time animations.</div>
           <div>
-            <Link href="./flexbox-game/level/level-1" className='text-xl' >
+            <Link href="./flexbox-game/level/level-1" className='text-md md:text-xl' >
             <StyleButton1 textToDisplay={myText}/>
             </Link>
           </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,7 +70,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased no-scrollbar`}
       >
         <Suspense fallback={<Loading />}>
           <Navbar />
