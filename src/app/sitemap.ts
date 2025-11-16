a05768078c3d387df055b083c07ee1552de41861
GulshanJha00 <gk4763549@gmail.com>
2025-11-16 12:01:14 +0530

    added in app folder

---
+++ src/app/sitemap.ts
@@ -0,0 +1,54 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://nieved.vercel.app",
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: "https://nieved.vercel.app/notes",
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: "https://nieved.vercel.app/practice",
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: "https://nieved.vercel.app/practice/first/question",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://nieved.vercel.app/practice/dsa",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://nieved.vercel.app/interview/aptitude/logical",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://nieved.vercel.app/chatroom/dsa",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://nieved.vercel.app/chatroom/dev",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://nieved.vercel.app/chatroom/ml",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://nieved.vercel.app/about",
+      lastModified: new Date(),
+      priority: 0.6,
+    },
+  ];
+}
