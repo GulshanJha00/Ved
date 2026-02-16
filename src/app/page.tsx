a707cd879d8d8238656a2556de59fc560f087b8b
GulshanJha00 <gk4763549@gmail.com>
2026-02-16 22:40:44 +0530

    error added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ const Page: React.FC = () => {
       <Details />
 
       <button
-  onClick={() => Sentry.showReportDialog()}
-  className="fixed bottom-6 right-6 backdrop-blur-md border-black
+  onClick={() => {throw new Error("Gupta sent error")}}
+  className="fixed bottom-6 right-6backdrop-blur-md border-black
  bg-white/70 border text-gray-900 px-5 py-3 rounded-full shadow-lg hover:bg-white hover:shadow-xl hover:scale-105 active:scale-95 transition-all duration-200 font-medium z-50"
 >
   🐞 Report Bug
