30a90bbedcd7643750ea5268a46cdd458bdd847b
GulshanJha00 <gk4763549@gmail.com>
2026-02-16 22:50:59 +0530

    error remved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const Page: React.FC = () => {
       <Details />
 
       <button
-  onClick={() => {throw new Error("Gupta sent error")}}
+  onClick={() => Sentry.showReportDialog()}
   className="fixed bottom-6 right-6backdrop-blur-md border-black
  bg-white/70 border text-gray-900 px-5 py-3 rounded-full shadow-lg hover:bg-white hover:shadow-xl hover:scale-105 active:scale-95 transition-all duration-200 font-medium z-50"
 >
